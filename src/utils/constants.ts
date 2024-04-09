3fa5190437847fcf0f295acb9015549aa51c537d
Chajac <chase.jackson@live.co.uk>
2024-04-09 22:47:16 +0100

    Utility helpers

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const NEW_DECK_SHUFFLED_URL =
+	"https://deckofcardsapi.com/api/deck/new/shuffle";
+export const DECK_URL = "https://deckofcardsapi.com/api/deck";
+export const CARD_BACK_IMG_URL =
+	"https://deckofcardsapi.com/static/img/back.png";
